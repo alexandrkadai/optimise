5f10aed7f6a4f8cead7ec3b67a0a37c8c29a5d04
Alex Kadai <alexandrkadai@gmail.com>
2022-12-25 10:51:03 +0200

    Starting optimisation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const togglePragraph = () => {
+    setShowParagraph((prevShowPraragraph) => !prevShowPraragraph);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={togglePragraph}> Show me the Money</Button>
     </div>
   );
 }
